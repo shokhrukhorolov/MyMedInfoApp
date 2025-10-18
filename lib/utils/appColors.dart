2c3744c770ef8cadc21794d40e63d64f4cae7424
shokhrukhorolov <shoxs5058@icloud.com>
2025-10-18 19:34:17 +0500

    Prepared colors and directories

---
+++ lib/utils/appColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/rendering.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF984039);
+  static const Color secondary = Color(0xFF586B3F);
+  static const Color background = Color(0xFFF1F1DC);
+  static const Color surface = Color(0xFFBCBBC1);
+  static const Color border = Color(0xFF8E8FA3);
+}
