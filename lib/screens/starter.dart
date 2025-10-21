4094f03c0e511c205844ca49422ad22e79895c4d
shokhrukhorolov <shoxs5058@icloud.com>
2025-10-21 18:09:26 +0500

    Started page

---
+++ lib/screens/starter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Starter extends StatelessWidget {
+  const Starter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
